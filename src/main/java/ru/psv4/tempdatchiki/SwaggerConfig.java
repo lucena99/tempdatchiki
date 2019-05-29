e0d1269ec49992b17d2f4622a2ac026849264f66
igr.kirillov@gmail.com <Igor.Kirillov@x5.ru>
2019-05-29 16:34:08 +0300

    swagger

--- src/main/java/ru/psv4/tempdatchiki/SwaggerConfig.java
+++ src/main/java/ru/psv4/tempdatchiki/SwaggerConfig.java
@@ -2,6 +2,8 @@ package ru.psv4.tempdatchiki;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -39,7 +41,7 @@ public class SwaggerConfig extends WebMvcConfigurerAdapter {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 //        registry
 //                .addResourceHandler("/swagger/**").addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("swagger-ui.html")
+        registry.addResourceHandler("/swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
 
         registry.addResourceHandler("/webjars/**")
